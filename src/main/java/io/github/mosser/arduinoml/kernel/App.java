07da8f8241b5b305f89fa0df513a4823df6494d4
Quentin cornevin <qcornevin@gmail.com>
2015-12-15 17:38:22 +0100

    Ajout des méthodes du DSL pour la déclarations des bricks / states / transitions de facon

--- src/main/java/io/github/mosser/arduinoml/kernel/App.java
+++ src/main/java/io/github/mosser/arduinoml/kernel/App.java
@@ -3,7 +3,9 @@ package io.github.mosser.arduinoml.kernel;
 import io.github.mosser.arduinoml.kernel.behavioral.State;
 import io.github.mosser.arduinoml.kernel.generator.Visitable;
 import io.github.mosser.arduinoml.kernel.generator.Visitor;
+import io.github.mosser.arduinoml.kernel.structural.Actuator;
 import io.github.mosser.arduinoml.kernel.structural.Brick;
+import io.github.mosser.arduinoml.kernel.structural.Sensor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,5 +54,44 @@ public class App implements NamedElement, Visitable {
 	@Override
 	public void accept(Visitor visitor) {
 		visitor.visit(this);
+	}
+
+
+	/**
+	 * all the method below are for the DSL
+     */
+
+	/**
+	 *
+	 *
+	 * @param name
+	 * @return
+     */
+	public App isNamed(String name) {
+		this.name = name;
+		return this;
+	}
+
+
+	public Actuator declareActuator() {
+		Actuator actuator = new Actuator();
+		this.bricks.add(actuator);
+		return actuator;
+	}
+
+	public Sensor declareSensor() {
+		Sensor sensor = new Sensor();
+		this.bricks.add(sensor);
+		return sensor;
+	}
+
+	public State declareState() {
+		State state = new State();
+		this.states.add(state);
+		return state;
+	}
+
+	public void declareInitialState(State state) {
+		this.initial = state;
 	}
 }
