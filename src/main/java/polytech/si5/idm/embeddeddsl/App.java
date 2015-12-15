614573c9f55e19558953535b22acbfacaf6f3a6b
Quentin cornevin <qcornevin@gmail.com>
2015-12-15 17:37:35 +0100

    Pas de code ici, on utilise la base de code de mosser

--- src/main/java/polytech/si5/idm/embeddeddsl/App.java
+++ src/main/java/polytech/si5/idm/embeddeddsl/App.java
@@ -4,10 +4,9 @@ package polytech.si5.idm.embeddeddsl;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
